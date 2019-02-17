bcafb73154de6ed381ca0d7867f3e79557ff7993
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-02-17 23:59:14 +0100

    - Background texture - Assets imported - Displaying text - Select child nodes - Change text

--- scripts/LoonyLips.gd
+++ scripts/LoonyLips.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 #Called when node is created
 func _ready():
-	var prompt = ["Jack", "car", "exicting"]
-	var story = "Today %s going to buy a new %s. It is so %s"
-	print(story % prompt)
+	var prompt = ["Jack", "car", "exiting"]
+	var story = "Today %s going to buy a new %s. It is so %s"	
+	$Blackboard/Story.bbcode_text = story % prompt
 	pass
