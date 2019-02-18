fd5179f4925afd506098b8a03d650092283b870d
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-02-18 23:22:09 +0100

    - LineEdit - TextureButton - RichTextLabel children that TextureButton - Signals -  Calling Functions

--- scripts/LoonyLips.gd
+++ scripts/LoonyLips.gd
@@ -5,4 +5,12 @@ func _ready():
 	var prompt = ["Jack", "car", "exiting"]
 	var story = "Today %s going to buy a new %s. It is so %s"	
 	$Blackboard/Story.bbcode_text = story % prompt
-	pass
+	$Blackboard/TextBox.clear()
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/Story.bbcode_text = new_text
+	$Blackboard/TextBox.clear()
+
+func _on_TextureButton_pressed():
+	var new_text = $Blackboard/TextBox.get_text()
+	_on_TextBox_text_entered(new_text)
