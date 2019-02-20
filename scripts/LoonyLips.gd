79acd15a3db402be38c6a4950b77ceeb5595c1d1
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-02-20 22:32:47 +0100

    - Removing nodes with queue_free() - Reloading scene with get_tree().reload_current_scene()

--- scripts/LoonyLips.gd
+++ scripts/LoonyLips.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 var story = "Today %s going to buy a new %s. It is so %s"	
-var prompt = ["a name", "a thing", "a feeling"]
+var prompt_words = ["a name", "a thing", "a feeling"]
 var player_words = []
 
 #Called when node is created
 func _ready():
 	$Blackboard/StoryText.bbcode_text = "Welcome to Loony Lips!!\n\n"
-	$Blackboard/StoryText.bbcode_text += "Can I have " + prompt[player_words.size()] + ", please?"
+	$Blackboard/StoryText.bbcode_text += "Can I have " + prompt_words[player_words.size()] + ", please?"
 	$Blackboard/TextBox.clear()
 
 #Called when press ENTER on TextBox
@@ -18,22 +18,33 @@ func _on_TextBox_text_entered(new_text):
 
 #Called when press TextureButton
 func _on_TextureButton_pressed():
-	var new_text = $Blackboard/TextBox.get_text()
-	_on_TextBox_text_entered(new_text)
+	if is_story_done():
+		if get_tree().reload_current_scene() != 0: #Reload the scene and check if some error happens
+			print("ERROR reloading the scene")
+	else:
+		var new_text = $Blackboard/TextBox.get_text()
+		_on_TextBox_text_entered(new_text)
 
 #Question for the player
 func prompt():
-	$Blackboard/StoryText.text = "Can I have " + prompt[player_words.size()] + ", please?"
+	$Blackboard/StoryText.text = "Can I have " + prompt_words[player_words.size()] + ", please?"
 
 func check_player_words_length():
-	if(player_words.size() == prompt.size()):
+	if is_story_done():
 		tell_story()
-		$Blackboard/TextBox.hide()
-		$Blackboard/TextureButton.hide()
 	else:
 		prompt()
+
+#Check if the story is ready to be told
+func is_story_done():
+	return player_words.size() == prompt_words.size()
 
 #Tell the complete story
 func tell_story():
 	$Blackboard/StoryText.text = story % player_words
+	end_game()
 
+#Change the elements state
+func end_game():
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/TextureButton/RichTextLabel.text = "Again!"
