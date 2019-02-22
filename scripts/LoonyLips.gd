d0f1216fb73602392fc4b1b686b7bc9b14b2d49f
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-02-22 01:03:55 +0100

    - Load static content from files, File object - Parse to JSON object - Added JSON files for stories and constant strings - No literals in the code

--- scripts/LoonyLips.gd
+++ scripts/LoonyLips.gd
@@ -1,33 +1,36 @@
 extends Node2D
 
-var template = [
-		{
-			"story" : "Today %s going to buy a new %s. It is so %s",
-			"prompt" : ["a name", "a thing", "a feeling"]
-		},
-		{
-			"story" : "A new enemy has arrived to %s. He is the most %s monster in the universe. Use the great %s to defeat him!",
-			"prompt" : ["a place", "a adjective", "a object"]
-		},
-		{
-			"story" : "The last %s match in this sesson is %s. I will %s while arrive it.",
-			"prompt" : ["a sport", "a time", "a action"]
-		}
-	]
 var current_story
 var player_words = []
+var STRINGS #location STRINGS (constants)
 
 #Called when node is created
 func _ready():
 	current_story = get_random_story()
-	$Blackboard/StoryText.bbcode_text = "Welcome to Loony Lips!!\n\n"
-	$Blackboard/StoryText.bbcode_text += "Can I have " + current_story.prompt[player_words.size()] + ", please?"
+	STRINGS = get_from_json("./other_STRINGS.json")
+	$Blackboard/StoryText.bbcode_text = STRINGS.welcome
+	$Blackboard/StoryText.bbcode_text += STRINGS.question % current_story.prompt[player_words.size()]
 	$Blackboard/TextBox.clear()
 
+#Return a ramdon story
 func get_random_story():
+	var stories = get_from_json("./stories.json")
 	randomize() #create a new random seed
-	var random_number = randi() % template.size()
-	return template[random_number]
+	var random_number = randi() % stories.size()
+	return stories[random_number]
+
+#Get the filename content like json object
+func get_from_json(filename):
+	var file = File.new()
+	var data = []
+	var result = file.open(filename, File.READ)
+	if result != 0:
+		print(STRINGS.error_open_file % filename)
+	else:
+		var text = file.get_as_text()
+		data = parse_json(text)
+		file.close()	
+	return data
 
 #Called when press ENTER on TextBox
 func _on_TextBox_text_entered(new_text):
@@ -39,15 +42,16 @@ func _on_TextBox_text_entered(new_text):
 func _on_TextureButton_pressed():
 	if is_story_done():
 		if get_tree().reload_current_scene() != 0: #Reload the scene and check if some error happens
-			print("ERROR reloading the scene")
+			print(STRINGS.error_reload_scene)
 	else:
 		var new_text = $Blackboard/TextBox.get_text()
 		_on_TextBox_text_entered(new_text)
 
 #Question for the player
 func prompt():
-	$Blackboard/StoryText.text = "Can I have " + current_story.prompt[player_words.size()] + ", please?"
+	$Blackboard/StoryText.text = STRINGS.question % current_story.prompt[player_words.size()]
 
+#Execute a action acording the size of player words
 func check_player_words_length():
 	if is_story_done():
 		tell_story()
@@ -66,4 +70,4 @@ func tell_story():
 #Change the elements state
 func end_game():
 	$Blackboard/TextBox.queue_free()
-	$Blackboard/TextureButton/RichTextLabel.text = "Again!"
+	$Blackboard/TextureButton/RichTextLabel.text = STRINGS.again
